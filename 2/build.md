e28a3f2f936404f91c4111b8d87a826e36935baa
d-- <d--@users.noreply.github.com>
2016-03-31 17:33:50 -0400

    added simple glfw program

---
+++ 2/build.md
@@ -0,0 +1,1 @@
+cc `pkg-config --cflags glfw3` -o main main.c `pkg-config --static --libs glfw3`
---
+++ 2/main.c
@@ -0,0 +1,43 @@
+#include <GLFW/glfw3.h>
+
+void key_handler(GLFWwindow* win, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_Q && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(win, GL_TRUE);
+    }
+}
+
+int main(void) {
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Register input callbacks */
+    glfwSetKeyCallback(window, key_handler);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        /* Render here */
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
