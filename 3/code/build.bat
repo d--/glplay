24f42844850ad6d6034b959588a833fd3ac165b6
d-- <d--@users.noreply.github.com>
2016-04-05 20:55:33 -0400

    clang on windows

--- 3/code/build.bat
+++ 3/code/build.bat
@@ -2,6 +2,6 @@
 mkdir ..\build
 pushd ..\build
 
-cl -Zi -MDd ..\code\glplay.c /IC:\dev\GLFW\include /IC:\dev\GLEW\include C:\dev\GLFW\lib\glfw3.lib C:\dev\GLEW\lib\Release\x64\glew32.lib User32.lib Gdi32.lib Shell32.lib Opengl32.lib
+clang ../code/glplay.c -IC:/dev/GLEW/include -IC:/dev/GLFW/include -lC:/dev/GLEW/lib/Release/x64/glew32.lib -lC:/dev/GLFW/lib/glfw3.lib -lUser32.lib -lGdi32.lib -lShell32.lib -lOpengl32.lib -lMSVCRTD.lib -llibcmt.lib -o glplay.exe
 
 popd
